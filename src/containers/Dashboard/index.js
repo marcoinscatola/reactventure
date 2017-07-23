20676ef77032924ea817eac7fd49c04ea1782a88
Marco Lepore <lepore86@gmail.com>
2017-07-23 18:06:38 +0200

    Remove notes module from the starter project

--- src/containers/Dashboard/index.js
+++ src/containers/Dashboard/index.js
@@ -1,18 +1,13 @@
 import React, {Component} from 'react';
 import Section from 'components/Section';
 import Page from 'components/Page';
-import NoteForm from './NoteForm';
-import NoteList from './NoteList';
 
 class Dashboard extends Component {
     render() {
         return (
             <Page>
                 <Section>
-                    <NoteForm/>
-                </Section>
-                <Section>
-                    <NoteList/>
+
                 </Section>
             </Page>
         )
