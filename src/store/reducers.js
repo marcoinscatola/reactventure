0c993531a0374807e6afba754fb7d7896b128fa8
Marco Lepore <lepore86@gmail.com>
2017-08-12 11:19:11 +0200

    Add i18n module
    Handles localization with messages and locale stored in the redux store, 
    displayed through <FormattedMessage> components

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,11 +1,13 @@
 import {combineReducers} from 'redux';
 import {authReducer as auth} from 'modules/auth';
 import {engineReducer as engine} from 'modules/engine';
+import {i18nReducer as i18n} from 'modules/i18n'
 import {logReducer as log} from 'modules/log';
 import {playerReducer as player} from 'modules/player';
 export default combineReducers({
     auth,
     engine,
+    i18n,
     log,
     player,
 });
